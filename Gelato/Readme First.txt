21aeeb832b2ba4a19623bb44b10ebb88ef4caabc
Brett Raper <72267696+techforchrist5227@users.noreply.github.com>
2022-12-23 13:46:17 -0500

    Add files via upload

---
+++ Gelato/Readme First.txt
@@ -0,0 +1,8 @@
+Hi,
+Thanks for Downloading our item
+
+if you want to ask something or ask for a license, 
+you can contact us via email : Vunira.project@gmail.com
+
+Thank you :)
+
--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const navtoggler = document.getElementsByClassName("navbar-toggler")[0];
 
-
-
 function toClear(){
     navtoggler.style.color="#FFF";
+} 
+
+const navToGreen = document.getElementsByClassName("nav-link")[navLinks]; 
+
+function navLinks(i){
+    for(let i = 0; i < "nav-link".length; i++)
+    document.getElementsByClassName("nav-link")[i];
+
+}
+// start the google map function for the shop
+function initMap() {
+    //var for shop coordinates
+    const cd_coords = {lat:35.10823, lng:-81.22934};
+    //map centered at clover donuts
+    const map = new google.maps.Map(document.getElementById("map"),{
+        zoom:4,
+        center: cd_coords,
+    }); 
+    //marker positioned at clover donuts
+    const marker = new google.maps.Marker({
+        position: cd_coords,
+        map:map,
+    });
 }
 
+window.initMap = initMap;
